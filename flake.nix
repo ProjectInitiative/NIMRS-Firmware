e4672f0231b7006fac0164df965316e5597db50c
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2026-02-08 23:05:41 -0600

    build: enforce correct partition size limit
    - Update buildFirmware script to set upload.maximum_size=2MB to match custom partition table.

--- flake.nix
+++ flake.nix
@@ -76,6 +76,7 @@
             --board-options "FlashSize=8M" \
             --board-options "PartitionScheme=default_8MB" \
             --build-property "build.partitions=$PWD/partitions.csv" \
+            --build-property "upload.maximum_size=2097152" \
             --output-dir build \
             --warnings default \
             .
