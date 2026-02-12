808101592008a65486ba71cd8be8c561d1b7dde1
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2026-02-12 15:21:54 -0600

    feat(audio): Implement Sound Asset Engine and CV Mapping

--- src/AudioController.h
+++ src/AudioController.h
@@ -4,11 +4,22 @@
 #include "SystemContext.h"
 #include "nimrs-pinout.h"
 #include <Arduino.h>
+#include <vector>
+#include <map>
 
 // Forward declarations
 class AudioOutputI2S;
 class AudioGenerator; // Base class
 class AudioFileSourceLittleFS;
+
+struct SoundAsset {
+  uint8_t id;
+  String name;
+  String type; // "simple", "complex_loop", "toggle"
+  String fileIntro;
+  String fileLoop;
+  String fileOutro;
+};
 
 class AudioController {
 public:
@@ -23,6 +34,9 @@ public:
   void playFile(const char *filename);
   void stop();
 
+  void loadAssets();
+  const SoundAsset* getAsset(uint8_t id);
+
 private:
   AudioController();
 
@@ -31,6 +45,8 @@ private:
   AudioFileSourceLittleFS *_file;
 
   bool _playing = false;
+  std::map<uint8_t, SoundAsset> _assets;
+  bool _lastFunctions[29] = {false};
 };
 
 #endif
