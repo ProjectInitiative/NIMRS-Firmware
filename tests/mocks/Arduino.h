b633ab240f7e62827bc82274697ed6cce93926e7
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2026-02-21 11:24:26 -0600

    chore: fmt

--- tests/mocks/Arduino.h
+++ tests/mocks/Arduino.h
@@ -99,6 +99,7 @@ public:
 };
 extern ESPClass ESP;
 
-#define map(x, in_min, in_max, out_min, out_max) ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+#define map(x, in_min, in_max, out_min, out_max)                               \
+  ((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
 
 #endif
