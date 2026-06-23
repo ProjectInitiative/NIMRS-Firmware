67e66d0f954f02ea9dcb119b1f082fc5a0b50ea3
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2026-06-23 00:01:42 +0000

    fix: switch from env_logger to println! for ESP-IDF compat; fix esp_get_idf_version fn name

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
 use std::ffi::CStr;
-use log::info;
 
 fn main() {
     esp_idf_sys::link_patches();
-    env_logger::builder()
-        .filter_level(log::LevelFilter::Info)
-        .init();
 
-    info!("NIMRS-Firmware (Rust) starting up...");
-    let version = unsafe { CStr::from_ptr(esp_idf_sys::esp_get_idf_version_str()) };
-    info!("ESP-IDF version: {}", version.to_string_lossy());
+    let version = unsafe { CStr::from_ptr(esp_idf_sys::esp_get_idf_version()) };
+    println!("NIMRS-Firmware (Rust) starting up...");
+    println!("ESP-IDF version: {}", version.to_string_lossy());
 
     loop {
         std::thread::sleep(std::time::Duration::from_secs(5));
-        info!("tick");
+        println!("tick");
     }
 }
