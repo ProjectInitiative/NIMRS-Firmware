d4b96fddda9c07f296ee340573149fee84aea6ff
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2026-06-24 16:12:36 +0000

    Use rustc-link-arg-bins for helix objects (avoids ldproxy -l conflict)

--- build.rs
+++ build.rs
@@ -6,7 +6,6 @@ fn main() {
 
     let out_dir = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
     let cc = "xtensa-esp32s3-elf-gcc";
-    let ar = "xtensa-esp32s3-elf-ar";
 
     let sources = [
         "mp3dec.c",
@@ -27,7 +26,6 @@ fn main() {
         "helix_compat.c",
     ];
 
-    let mut objects = Vec::new();
     for src in &sources {
         let src_path = format!("vendor/libhelix/{}", src);
         let obj = out_dir.join(src.replace('/', "_").replace(".c", ".o"));
@@ -46,26 +44,9 @@ fn main() {
             ])
             .arg(obj.to_str().unwrap())
             .status()
-            .expect("failed to compile helix source");
-        if !status.success() {
-            panic!("helix compilation failed for {}", src);
-        }
-        objects.push(obj);
+            .expect("helix compile failed");
+        assert!(status.success(), "helix compile failed for {}", src);
+        // Emit object file directly as a linker argument (avoids -l flag which confuses ldproxy)
+        println!("cargo:rustc-link-arg-bins={}", obj.to_str().unwrap());
     }
-
-    // Create static library archive manually
-    let lib_path = out_dir.join("libhelix-mp3.a");
-    let mut ar_cmd = std::process::Command::new(ar);
-    ar_cmd.arg("crs").arg(lib_path.to_str().unwrap());
-    for obj in &objects {
-        ar_cmd.arg(obj.to_str().unwrap());
-    }
-    let status = ar_cmd.status().expect("failed to create helix archive");
-    if !status.success() {
-        panic!("ar failed");
-    }
-
-    // Use -l: to specify the library directly (avoids -l static= format that confuses ldproxy)
-    println!("cargo:rustc-link-search=native={}", out_dir.display());
-    println!("cargo:rustc-link-lib=helix-mp3");
 }
